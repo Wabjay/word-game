9cbe94ea0fd8a5a9281eb08214335c635a9628b7
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2023-12-07 11:52:12 -0600

    initial commit

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,19 +1,30 @@
-import { options } from "../api/auth/[...nextauth]/options"
-import { getServerSession } from "next-auth/next"
-import UserCard from "../components/UserCard"
-import { redirect } from "next/navigation"
+// import { options } from "../api/auth/[...nextauth]/options"
+// import { getServerSession } from "next-auth/next"
+// import UserCard from "../components/UserCard"
+// import { redirect } from "next/navigation"
 
-export default async function ServerPage() {
-    const session = await getServerSession(options)
+// export default async function ServerPage() {
+//     const session = await getServerSession(options)
 
-    if (!session) {
-        redirect('/api/auth/signin?callbackUrl=/server')
-    }
+//     if (!session) {
+//         redirect('/api/auth/signin?callbackUrl=/server')
+//     }
 
-    return (
-        <section className="flex flex-col gap-6">
-            <UserCard user={session?.user} pagetype={"Server"} />
-        </section>
-    )
+//     return (
+//         <section className="flex flex-col gap-6">
+//             <UserCard user={session?.user} pagetype={"Server"} />
+//         </section>
+//     )
 
+// }
+
+
+import React from 'react'
+
+const ServerPage = () => {
+  return (
+    <div>ServerPage</div>
+  )
 }
+
+export default ServerPage
